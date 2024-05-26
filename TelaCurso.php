437de33f95dd3b4688883c1f32942e4585c36424
MariaCla14 <133056426+MariaCla14@users.noreply.github.com>
2024-05-26 12:39:28 -0300

    TelaCurso.php

---
+++ TelaCurso.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estiloHome.css"/>
+
+    <title>Guia JK</title>
+</head>
+<body>
+<nav>
+    <p id="guia">Guia</p>
+    <h1 id="jk">JK</h1>
+    <div class="dropdown dropdown1">
+        <img id="imgPerfil" src="data:image/png"/>
+        <div class="dropdown-content">
+            <div class="btn"><p id="pf"><a href="Telalogin.php">Login</a></p></div>
+            <div class="btn"><p id="pf"><a href="TelaCadastro.php">Cadastrar-se</a></p></div>
+            <section id="secdd">
+                <p id="pi">Entrar como <br>ADM</p>
+            </section>
+
+        </div>
+    </div>
+
+    <section id="link">
+        <a id="at" href="#">Teste Vocacional</a>
+        <a id="at" href="#sec2">Mapa Escolar</a>
+        <a id="at" href="#sec3">Sobre os Cursos</a>
+        <a id="at" href="#sec4">Sobre a Escola</a>
+    </section>
+</nav>
+
+<section id="sec1">
+    <br>
+    <br>
+    <h2 id="tv">CURSOS</h2>
+    <p><strong> JK temos 5 opções de cursos diferentes, venha conosco saber mais sobre elas!</strong></p>
+    <p>Turismo para os exploradores, Administração para os mais lógicos, Análises para os apaixonados pela área de saúde,<br> e Eletrotécnica e Informática para os entusiastas das áreas exatas. :D</p>
+</section>
+
+<section id="sec2">
+    <br>
+    <h2>Análises Clínicas</h2>
+    <P>O curso de Análises Clínicas capacita profissionais para realizar análises laboratoriais em amostras biológicas, como sangue e urina, <br> utilizando técnicas variadas.  Os estudantes aprendem sobre biologia, química, hematologia, microbiologia, imunologia e bioquímica clínica, <br> além de gestão de qualidade e biossegurança. Os graduados podem trabalhar em laboratórios clínicos, hospitais, indústrias farmacêuticas<br> e de biotecnologia, contribuindo para diagnósticos precisos e tratamentos eficazes.</P>
+    <img src="anals.jpg" alt="Descrição da imagem 3" style="width: 100%; border-radius: 10px;">
+</section>
+
+<section id="sec3">
+    <br>
+    <h2>Turismo</h2>
+    <p>Um curso técnico em Turismo abrange uma ampla gama de tópicos, desde gestão de destinos e hospitalidade até agenciamento de viagens<br>. Os alunos aprendem habilidades práticas e conhecimentos teóricos essenciais para trabalhar em diversas áreas da indústria do turismo,<br> como agências de viagens, hotéis, eventos e atrações turísticas. O curso prepara os estudantes para enfrentar os desafios dinâmicos<br> e éticos do setor, contribuindo para o crescimento sustentável e a promoção de experiências turísticas enriquecedoras.</p>
+   
+</section>
+
+</body>
+</html>
